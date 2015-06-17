8b86d3e3078b8de75a10762ac3f34a414d450ff6
namemealrady <Staff@github.com>
2015-06-17 10:33:56 +0700

    background.js
    Jim

--- background.js
+++ background.js
@@ -9,6 +9,20 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.sendMessage(tab.id, {action: 'toggle', showingRealNames: showingRealNames });
 });
 
+<p>Search a string for "function", and display the position of the match:</p>
+
+<button onclick="myFunction()">and go</button>
+
+<p id="Path"></p>
+
+<script>
+function myFunction() {
+    var str = "Visit W3Schools!"; 
+    var n = str.search(/w3Schools/i);
+    document.getElementById("Path").innerHTML = n;
+}
+</script>
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action == "get-showingRealNames") {
       var showingRealNames = localStorage['showingRealNames'];
